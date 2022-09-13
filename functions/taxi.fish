8645174a5a1d3c0662f00d6789286eb501e8ba16
Sriram Venkatesh <sriram.venkatesh@shellenergy.com.au>
2022-09-13 14:45:39 +1000

    fix: 🐛 timesheet file exist between invocations

--- functions/taxi.fish
+++ functions/taxi.fish
@@ -4,6 +4,7 @@ function taxi --description "a timesheeting cli tool"
 	--rm \
 	-v "$HOME/.config/taxi:/root/.config/taxi" \
 	-v "$HOME/.local/share/taxi:/root/.local/share/taxi" \
+	-v "$HOME/zebra/:/root/zebra/" \
 	ghcr.io/srizzling/taxi-docker:main \
 	taxi $argv
 end
