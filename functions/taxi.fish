ec9811e9db52accd36aece3d92a196642a3f393c
Sriram Venkatesh <sriram.venkatesh@shellenergy.com.au>
2022-09-13 14:28:51 +1000

    fix: 🐛 project exist between invocations

--- functions/taxi.fish
+++ functions/taxi.fish
@@ -3,6 +3,7 @@ function taxi --description "a timesheeting cli tool"
 	-it \
 	--rm \
 	-v "$HOME/.config/taxi:/root/.config/taxi" \
+	-v "$HOME/.local/share/taxi:/root/.local/share/taxi" \
 	ghcr.io/srizzling/taxi-docker:main \
 	taxi $argv
 end
